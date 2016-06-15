8cd72adfa7997ebf772013805fd22c2cc20d4b2d
aderjaan <aderjaan.vandenberg@changer.nl>
2016-06-15 13:46:41 +0200

    add support for ssl in mongo url

--- db/connect.go
+++ db/connect.go
@@ -1,8 +1,12 @@
 package db
 
 import (
+	"crypto/tls"
 	"fmt"
+	"net"
 	"os"
+	"strings"
+	"time"
 
 	"gopkg.in/mgo.v2"
 )
@@ -29,14 +33,42 @@ func Connect() {
 		uri = MongoDBUrl
 	}
 
-	mongo, err := mgo.ParseURL(uri)
-	s, err := mgo.Dial(uri)
-	if err != nil {
-		fmt.Printf("Can't connect to mongo, go error %v\n", err)
-		panic(err.Error())
-	}
-	s.SetSafe(&mgo.Safe{})
+	Session, Mongo = getNewSession(uri)
 	fmt.Println("Connected to", uri)
-	Session = s
-	Mongo = mongo
+}
+
+func getNewSession(connString string) (*mgo.Session, *mgo.DialInfo) {
+	// quick hack to allow SSL based connections, may be removed in future when parseURL supports it
+	// see also: https://github.com/go-mgo/mgo/issues/84
+	const SSL_SUFFIX = "?ssl=true"
+	useSsl := false
+
+	if strings.HasSuffix(connString, SSL_SUFFIX) {
+		connString = strings.TrimSuffix(connString, SSL_SUFFIX)
+		useSsl = true
+	}
+
+	dialInfo, err := mgo.ParseURL(connString)
+	if err != nil {
+		panic(err)
+	}
+
+	dialInfo.Timeout = 10 * time.Second
+
+	if useSsl {
+		config := tls.Config{}
+		config.InsecureSkipVerify = true
+
+		dialInfo.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
+			return tls.Dial("tcp", addr.String(), &config)
+		}
+	}
+
+	// get a mgo session
+	session, err := mgo.DialWithInfo(dialInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	return session, dialInfo
 }
